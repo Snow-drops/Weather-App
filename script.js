f43c5c3267b40a8ce1d3a56fa7992656066027b9
sanjanamasam1210 <sanjanamasam1210@gmail.com>
2023-06-16 03:57:26 +0530

    Co-authored-by: Adnan Siddiqui <adn-sdq@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,22 @@
+const api{
+    key:"339e8964c3a9c3d9855ae9d61a5cb452",
+    base:"https://api.openweathermap.org/data/2.5/"
+}
+
+const search= document.querySelector(".search-box");
+const btn= document.querySelector(".btn");
+btn.addEventListener('click', getInput);
+
+function getInput(event){
+    event.preventDefault();
+    if(event.type=="click"){
+        getData(search.value);
+        console.log(search.value);
+    }
+}
+
+function getData(){
+    fetch(`${api.base}weather?q=${search.value}&units=metric&APPID=${api.key}`)
+    .then(response=>response.json())
+    .then(displayData);    
+}
